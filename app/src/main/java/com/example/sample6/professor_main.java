640060d790561fc0b0aec85206f0d3d813b329e7
Mkarthik1234 <karthikshettigarm@gmail.com>
2022-12-18 14:53:54 +0530

    added scroll view to student main

--- app/src/main/java/com/example/sample6/professor_main.java
+++ app/src/main/java/com/example/sample6/professor_main.java
@@ -19,9 +19,35 @@ public class professor_main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_professor_main);
+        setContentView(R.layout.professor_main);
 
         auth = FirebaseAuth.getInstance();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item_logout_opmenu:
+                auth.signOut();
+                removeSharedPreferese("999");
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+    private void removeSharedPreferese(String no) {
+        SharedPreferences ref = getSharedPreferences("login",MODE_PRIVATE);
+        SharedPreferences.Editor editor = ref.edit();
+        editor.putString("isProfessor",no);
+        editor.apply();
     }
 }
