eb3c072d706b6552e43fe941616ce344d1c799b3
Mkarthik1234 <karthikshettigarm@gmail.com>
2022-12-18 14:08:03 +0530

    added scroll view to sign up and registration pages

--- app/src/main/java/com/example/sample6/student_main.java
+++ app/src/main/java/com/example/sample6/student_main.java
@@ -9,17 +9,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class student_main extends AppCompatActivity {
     FirebaseAuth auth;
+    ImageView time_table,subjects,marks,atendese;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main);
-
         auth = FirebaseAuth.getInstance();
+
+        time_table = findViewById(R.id.img_attendense_stuhome);
+        subjects = findViewById(R.id.img_subjects_stuhome);
+        marks = findViewById(R.id.img_marks_stuhome);
+        atendese = findViewById(R.id.img_attendense_stuhome);
+
+
 
     }
     @Override
